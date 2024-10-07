c3d8970fafb552d372cc40cb16ae64edaeae208f
Nagendra Kumar Sharma <97817754+Developernagendra@users.noreply.github.com>
2024-10-07 13:25:47 +0000

    #

---
+++ 01_basics/03_conversion.js
@@ -0,0 +1,31 @@
+let score="hagvcj123";
+
+console.log(typeof score);
+console.log(typeof(score))
+
+let valueInNumber=Number(score)
+console.log(typeof valueInNumber)
+console.log(valueInNumber);
+
+
+// "123"=>123
+//"123abc"=>NaN
+//true=>1 // false=>0
+
+let isLoggedIn="12345"
+
+let booleanIsLoggedIn=Boolean(isLoggedIn)
+console.log(booleanIsLoggedIn);
+
+let someNumber=33
+
+let stringNumber=String(someNumber)
+console.log(stringNumber);
+console.log(typeof(stringNumber));
+
+
+
+
+
+
+
