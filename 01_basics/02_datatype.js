78bd0284b55354992b45de9aee034037753e65a2
Nagendra Kumar Sharma <97817754+Developernagendra@users.noreply.github.com>
2024-10-07 12:59:00 +0000

    #

---
+++ 01_basics/02_datatype.js
@@ -0,0 +1,30 @@
+"use strict";//treat  all js code as newer version
+
+//alert("hello") // we are using node js not browser
+
+console.log(3+3);
+console.log("nagendra");
+
+let name="nagendra"
+let age=18;
+let isLoggedIn=false
+let state;
+
+//  premitive data types
+//number=>2 to power 53
+//bigint
+//string=""
+//boolean=>true/false
+//null=>standalone value
+//undefined=>
+//symbol =>unique
+
+// non premetive datatype
+//object
+
+console.log(typeof null); // object
+console.log(typeof undefined); //undefined
+
+
+
+
