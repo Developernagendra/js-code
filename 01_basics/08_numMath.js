eeab69f625177ae4c28e355e178c1ea5a8fc9e66
Nagendra Kumar Sharma <97817754+Developernagendra@users.noreply.github.com>
2024-10-08 08:23:12 +0000

    numMath

---
+++ 01_basics/08_numMath.js
@@ -0,0 +1,59 @@
+const score=400
+// console.log(score);
+
+
+const balance=new Number(100)
+// console.log(balance);
+
+// console.log(balance.toString().length);
+// console.log(balance.toFixed(2));
+
+const otherNumber=1224.8978675
+// console.log(otherNumber.toPrecision(4));
+
+const hundres=1000000
+// console.log(hundres.toLocaleString('en-IN'));
+
+//**************MATHS*************** */
+
+console.log(Math);
+// console.log(Math.abs(-3));
+// console.log(Math.round(4.3));
+// console.log(Math.ceil(4.2));
+// console.log(Math.floor(4.9));
+// console.log(Math.min(1,2,4,8));
+
+console.log(Math.random());
+console.log(Math.random()*10);
+
+const min=10
+const max=20
+
+console.log(Math.floor(Math.random() * (max -min+1))+min)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
