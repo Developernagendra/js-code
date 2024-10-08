194c15b1644f1848b85d004de5563019cbc44229
Nagendra Kumar Sharma <97817754+Developernagendra@users.noreply.github.com>
2024-10-08 14:14:41 +0000

    Array

---
+++ 02_basics/01_Array.js
@@ -0,0 +1,48 @@
+// Array
+
+const myArr=[1,2,3,4,5,6,]
+const myHeros=["shaktiman","nagin"]
+
+// console.log(myArr[0]);
+// console.log(myHeros);
+
+
+// array method
+
+// myArr.push(6)
+// myArr.push(10)
+// myArr.pop(10)
+
+// myArr.unshift(9)
+// myArr.shift()
+
+// console.log(myArr.includes(9));
+// console.log(myArr.indexOf(3));
+
+
+// const newArr=myArr.join()
+// console.log( typeof newArr);
+// console.log(myArr);
+
+
+// slice or splice
+
+console.log("A" ,myArr);
+
+const myn1=myArr.slice(1,3);
+
+console.log(myn1);
+console.log("B" ,myArr);
+
+const mya2=myArr.splice(1,3)
+console.log("C",myArr);
+console.log(mya2);
+
+
+
+
+
+
+
+
+
