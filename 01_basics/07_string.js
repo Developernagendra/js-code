55ca0884a925566b646f10308ccad8cbd9d4b181
Nagendra Kumar Sharma <97817754+Developernagendra@users.noreply.github.com>
2024-10-08 07:56:39 +0000

    string

---
+++ 01_basics/07_string.js
@@ -0,0 +1,61 @@
+const name="naggu"
+const repoCount=50
+
+//console.log(name+repoCount+"value");
+
+console.log(`Hello my name is ${10} and my repo count is ${29}`);
+
+const gameName=new String('naggu-gc')
+
+console.log(gameName[0]);
+console.log(gameName.__proto__);
+
+console.log(gameName.length);
+console.log(gameName.toLocaleLowerCase());
+console.log(gameName.charAt());
+console.log(gameName.endsWith());
+console.log(gameName.indexOf('u'));
+
+const newString=gameName.substring(0,4)
+console.log(newString);
+
+const anotherstring=gameName.slice(-8 ,4)
+console.log(anotherstring);
+
+const newString1="   naggu   "
+console.log(newString1);
+console.log(newString1.trim());
+
+const url="https://techbloggerworld.com"
+
+console.log(url.replace('%20','-'));
+console.log(url.includes('blog'));
+console.log(url.includes('beer'));
+
+console.log(gameName.split('-'));
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
